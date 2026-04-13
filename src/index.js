89872a7c4bdff544a213ebaa72f8bf4e7fc2e568
Sai <sai3cs@gmail.com>
2026-04-13 15:51:51 +0000

    Add GitHub Actions workflow for Docker EC2 deploynew2

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 80;
+
+app.get('/', (req, res) => {
+  res.send('Hello Dinesh! 🚀 Your Docker EC2 deployment is working.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
